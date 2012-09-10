5589020afbcf703ca932881845c3f0b199e87aac
Stian Liknes <stianlik@gmail.com>
2012-09-10 10:26:03 +0200

    Refactored rules for admin controller

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -24,13 +24,8 @@ class AdminController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete','create','update','view'),
-				'users'=>UserModule::getAdmins(),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
+			array('allow','users'=>UserModule::getAdmins()),
+			array('deny'),
 		);
 	}
 	/**
