30df054cc534b291b0cbfd3e254536d7bbb8c5b1
Stian Liknes <stianlik@gmail.com>
2012-09-10 09:01:57 +0200

    Profile page as start page

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -3,23 +3,9 @@
 class DefaultController extends Controller
 {
 	
-	/**
-	 * Lists all models.
-	 */
 	public function actionIndex()
 	{
-		$dataProvider=new CActiveDataProvider('User', array(
-			'criteria'=>array(
-		        'condition'=>'status>'.User::STATUS_BANNED,
-		    ),
-			'pagination'=>array(
-				'pageSize'=>Yii::app()->controller->module->user_page_size,
-			),
-		));
-
-		$this->render('/user/index',array(
-			'dataProvider'=>$dataProvider,
-		));
+		$this->redirect($this->createAbsoluteUrl("/{$this->module->id}/profile"));
 	}
 
 }
