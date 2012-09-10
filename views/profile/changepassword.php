521643147b4ff50308930efdd5ef683afeb516ea
Stian Liknes <stianlik@gmail.com>
2012-09-10 10:23:06 +0200

    Fixed menu structure

--- views/profile/changepassword.php
+++ views/profile/changepassword.php
@@ -4,10 +4,8 @@ $this->breadcrumbs=array(
 	UserModule::t("Change Password"),
 );
 $this->menu=array(
-	((UserModule::isAdmin())
-		?array('label'=>UserModule::t('Manage Users'), 'url'=>array('/user/admin'))
-		:array()),
-    array('label'=>UserModule::t('List User'), 'url'=>array('/user')),
+	array('label'=>UserModule::t('Manage Users'), 'url'=>array('/user/admin'), 'visible' => UserModule::isAdmin()),
+    array('label'=>UserModule::t('List User'), 'url'=>array('/user'), 'visible' => UserModule::isAdmin()),
     array('label'=>UserModule::t('Profile'), 'url'=>array('/user/profile')),
     array('label'=>UserModule::t('Edit'), 'url'=>array('edit')),
     array('label'=>UserModule::t('Logout'), 'url'=>array('/user/logout')),
